02771b8ce51a6d3c98cef69d79097aba69be773f
abhaydixit07 <abhaydixit227@gmail.com>
2024-04-30 22:12:46 +0530

    Final Frontend

---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import ChannelContainer from "./ChannelContainer";
+import ChannelListContainer from "./ChannelListContainer";
+import ChannelSearch from "./ChannelSearch";
+import TeamChannelList from "./TeamChannelList";
+import TeamChannelPreview from "./TeamChannelPreview";
+import TeamMessage from "./TeamMessage";
+import ChannelInner from "./ChannelInner";
+import CreateChannel from "./CreateChannel";
+import EditChannel from "./EditChannel";
+import Auth from "./Auth";
+import UserList from "./UserList";
+import ResultsDropdown from "./ResultsDropdown";
+export  {
+    ChannelContainer,
+    ChannelListContainer,
+    ChannelSearch, 
+    TeamChannelList, 
+    TeamChannelPreview, 
+    TeamMessage,
+    EditChannel,
+    ChannelInner,
+    CreateChannel,
+    Auth, 
+    UserList,
+    ResultsDropdown
+
+};
